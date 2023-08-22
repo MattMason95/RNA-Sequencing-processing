7c92e95ebfde6b4729abca8624c35fb114511cca
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-08-22 13:22:36 +0100

    Update 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 # AUTHOR: Matthew Mason
 # EDITED: 10.2023
-# DESC: Full assembly pipeline of BASH scripts for RNA-seq read alignment and quantification
+# DESC: Pipeline module for Trimmomatic trimming of reads
+
+## SBATCH parameters
+#SBATCH -p skylake-himem
+#SBATCH --J mm2458_Trimmomatic
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --output=output_STAR_smp_4
+#SBATCH --mem=10gb
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=mm2458@cam.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --output=
+#SBATCH --error=
+#SBATCH --no-requeue
 
 echo "Read Trimming {01_trimmomatic.sh}" # Declare start of trimming process
+
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR cd $SLURM_SUBMIT_DIR
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+# <><><>><><><><><><><><><><><><><><><><><><><><>
+# <> BUILD DEFAULT ENVIRONMENT                 <>
+# <><><>><><><><><><><><><><><><><><><><><><><><>
+
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel7/default-peta4            # REQUIRED - loads the basic environment
 
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 # <> READ TRIMMING                             <>
@@ -29,7 +57,7 @@ do
   fq1=${base}_1.fq.gz # Instantiate R1 singleton
   fq2=${base}_2.fq.gz # Instantiate R2 singleton (PAIRED)
 
-  output=${base_}
+  output=${base}
 
   # Run trimmomatic
   java -jar "$TRIMMOMATIC" \
@@ -49,13 +77,12 @@ do
       MINLEN:36
 
   # Option to remove the unpaired files
-  rm $trim/${output}_R1.U.fastq.gz \
-     $trim/${output}_R2.U.fastq.gz
-
+  # rm $trim/${output}_R1.U.fastq.gz \
+  #    $trim/${output}_R2.U.fastq.gz
 
 done
 
 ## LOGFILE
-sh ./AL_Logs.sh "log" "Trimmomatic" "$trim"
+# sh ./AL_Logs.sh "log" "Trimmomatic" "$trim"
 
 ## FIN
