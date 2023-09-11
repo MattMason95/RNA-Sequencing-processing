d4f50a0f944b4ba856d330332f275cfe54882e04
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-09-11 20:02:31 +0100

    Update 00_Master_Pipeline.sh

--- 00_Master_Pipeline.sh
+++ 00_Master_Pipeline.sh
@@ -12,17 +12,23 @@
 ## - 
 ## - 
 
+# ~~~~~~~~~~ DEFAULT REQUIREMENTS ~~~~~~~~~~
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+
+# ~~~~~~~~~~ PIPELINE SPECIFIC ~~~~~~~~~~
 module load $HOME/rds/hpc-work/Software/STAR
 module load $HOME/rds/hpc-work/Software
 
-TRIMMOMATIC = "$HOME/rds/hpc-work/Software/trimmomatic-0.39.jar"
+TRIMMOMATIC="$HOME/rds/hpc-work/Software/trimmomatic-0.39.jar"
 
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 # <> INPUT VARIABLE DECLARATION                <>
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 ## A space to instantiate additional input arguments from the .SBATCH EXECUTE script 
 
-nTasks = $1 # Number of threads allocated to the entire pipeline at submission
+nTasks=$1 # Number of threads allocated to the entire pipeline at submission
 
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 # <> DIRECTORY ALLOCATION                      <>
@@ -30,16 +36,20 @@ nTasks = $1 # Number of threads allocated to the entire pipeline at submission
 ## Modifiable directory allocation for data read/write
 
 # ~~~~~~~~~~ GENERAL ~~~~~~~~~~
-raw = "$HOME/rds/hpc-work/Data/Fasta_files"  # Location of the raw Fastq files
+raw="$HOME/rds/hpc-work/Data/Fasta_files"  # Location of the raw Fastq files
 
 # ~~~~~~~~~~ TRIMMOMATIC ~~~~~~~~~~
-adapters = "$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa" # Location of Illumina adapter sequences for clipping
-trimmed = "$HOME/rds/hpc-work/Data/Trimmed"                                      # Location to write/read trimmed Fastq files 
+adapters="$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa" # Location of Illumina adapter sequences for clipping
+trimmed="$HOME/rds/hpc-work/Data/Trimmed"                                      # Location to write/read trimmed Fastq files 
 
 # ~~~~~~~~~~ STAR ~~~~~~~~~~
-genome = "$HOME/rds/hpc-work/Data/Genome"            # Location of the importred genome build for indexing
-                                                     # At the time of running, Mus musculus genome build: 
-genomeIndex = "$HOME/rds/hpc-work/Data/Genome/Index" # Location of the indexed genome
+genome="$HOME/rds/hpc-work/Data/Genome"            # Location of the importred genome build for indexing
+                                                   # At the time of running, Mus musculus genome build: ____
+genomeIndex="$HOME/rds/hpc-work/Data/Genome/Index" # Location of the indexed genome
+bam="$HOME/rds/hpc-work/Data/BAM"                  #Location to write BAM files
+fpkm="$HOME/rds/hpc-work/Data/FPKM"                #Location to write FPKM files
+ctab="$HOME/rds/hpc-work/Data/CTAB"                #Location to write CTAB files
+
 
 
 
@@ -48,9 +58,9 @@ genomeIndex = "$HOME/rds/hpc-work/Data/Genome/Index" # Location of the indexed g
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 
 # ~~~~~~~~~~ TRIMMOMATIC ~~~~~~~~~~
-adapters = "$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"
+adapters="$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"
 
-cmd1 = "Scripts/02_trimmomatic.sh $raw $trimmed $adapters $nTasks"
+cmd1="Scripts/02_trimmomatic.sh $raw $trimmed $adapters $nTasks"
 echo $cmd1 
 eval $cmd1 
 
@@ -58,7 +68,7 @@ eval $cmd1
 
 # ~~~~~~~~~~ STAR ~~~~~~~~~~
 
-cmd2 = "Scripts/03_read_alignment.sh $trimmed $trimmed $adapters $nTasks"
+cmd2="Scripts/03_read_alignment.sh $trimmed $trimmed $adapters $nTasks"
 echo $cmd2 
 eval $cmd2 
 
