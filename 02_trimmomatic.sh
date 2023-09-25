0ce8f7659eaa0b2d65c260eafd704061c169cf70
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-09-25 12:32:28 +0100

    Update 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -1,30 +1,19 @@
-echo "Read Trimming {01_trimmomatic.sh}" # Declare start of trimming process
-
-# <><><>><><><><><><><><><><><><><><><><><><><><>
-# <> BUILD DEFAULT ENVIRONMENT                 <>
-# <><><>><><><><><><><><><><><><><><><><><><><><>
-
-. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
-module purge                               # Removes all modules still loaded
-module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+echo "Read Trimming {02_trimmomatic.sh}" # Declare start of trimming process
 
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 # <> READ TRIMMING                             <>
 # <><><>><><><><><><><><><><><><><><><><><><><><>
 
 ## PASSED VARIABLES
-
-raw=$1         #Location to read raw reads
-trim=$2        #Location to write trimmed reads
-adapters=$3    #Location to read adapaters
-nTasks=$4   #Naming convention for raw reads
+raw="$HOME/rds/hpc-work/Data/Fasta_files"                                        #Location to read raw reads
+trim="$HOME/rds/hpc-work/Data/Trimmed"                                           #Location to write trimmed reads
+adapters="$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"   #Location to read adapaters
+nTasks=$1                                                                        #Naming convention for raw reads
 
 ## TRIMMOMATIC LOCATION
-
 TRIMMOMATIC="$HOME/rds/hpc-work/Software/trimmomatic-0.39.jar"
 
 ## EXECUTION
-
 for infile in $raw/*_1.fq.gz
 do
 
@@ -42,14 +31,7 @@ do
       -basein $raw/${fq1} $raw/${fq2} \
       -baseout $trim/${output}_R1p.trimmed.fastq.gz $trim/${output}_R1u.trimmed.fastq.gz $trim/${output}_R2p.trimmed.fastq.gz $trim/${output}_R2u.trimmed.fastq.gz \
       ILLUMINACLIP:"$adapters":2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-
-  # Option to remove the unpaired files
-  # rm $trim/${output}_R1.U.fastq.gz \
-  #    $trim/${output}_R2.U.fastq.gz
-
 done
-
-## LOGFILE
-# sh ./AL_Logs.sh "log" "Trimmomatic" "$trim"
+wait
 
 ## FIN
