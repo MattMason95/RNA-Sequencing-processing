7666cd0896b70f7b4c661f14e1dbb3aa214ebf4e
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-09-26 09:49:48 +0100

    Update 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -21,16 +21,16 @@ do
         fq1=$(basename "${file1}")
         fq2=$(basename "${file2}")
         base=${fq1%%_1.fq.gz}
-
-        echo $fq1
-        echo $fq2
-        echo $base
         
-        CHECKFILE="$trim/${base}_R1u.trimmed.fastq.gz"
+        CHECKFILE="${base}_R1u.trimmed.fastq.gz"
 
-        if [-f "$CHECKFILE"]; then
+        if [ -f $trim/$CHECKFILE ]; then
                 echo "$CHECKFILE exists. Skipping file."
         else
+        
+                echo $fq1
+                echo $fq2
+                echo $base
                 ## Run trimmomatic
                 java -jar "$TRIMMOMATIC" PE $raw/$fq1 $raw/$fq2 $trim/${base}_R1p.trimmed.fastq.gz $trim/${base}_R1u.trimmed.fastq.gz $trim/${base}_R2p.trimmed.fastq.gz $trim/${base}_R2u.trimmed.fastq.gz ILLUMINACLIP:"$adapters":2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
         fi 
