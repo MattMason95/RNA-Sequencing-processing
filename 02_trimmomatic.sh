693494bb501ecb2303fce38dbb1e2914756bf829
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-09-29 11:21:36 +0100

    Update 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -8,33 +8,31 @@ echo "Read Trimming {02_trimmomatic.sh}" # Declare start of trimming process
 raw="$HOME/rds/hpc-work/Data/Fasta_files"                                        #Location to read raw reads
 trim="$HOME/rds/hpc-work/Data/Trimmed"                                           #Location to write trimmed reads
 adapters="$HOME/rds/hpc-work/Software/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"   #Location to read adapaters
-nTasks=$1                                                                        #Naming convention for raw reads
+slurmID=$1                                                                        #Naming convention for raw reads
 
 ## TRIMMOMATIC LOCATION
 TRIMMOMATIC="$HOME/rds/hpc-work/Software/trimmomatic-0.39.jar"
 
+## SAMPLE CSV FILE
+SAMPLES="$HOME/rds/hpc-work/Data/Sample_ID.csv"
+
 ## EXECUTION
-for file1 in $raw/*_1.fq.gz
-do
-        file2=${file1%%_1.fq.gz}"_2.fq.gz" # Instantiate R2 singleton (PAIRED)
+sample=$(head -n $slurmID $SAMPLES | tail -n 1 | cut -d "," -f 1)
+fq1=$(head -n $slurmID $SAMPLES | tail -n 1 | cut -d "," -f 2)
+fq2=$(head -n $slurmID $SAMPLES | tail -n 1 | cut -d "," -f 3)
 
-        fq1=$(basename "${file1}")
-        fq2=$(basename "${file2}")
-        base=${fq1%%_1.fq.gz}
-        
-        CHECKFILE="${base}_R1u.trimmed.fastq.gz"
+base=${fq1%%_1.fq.gz}
 
-        if [ -f $trim/$CHECKFILE ]; then
-                echo "$CHECKFILE exists. Skipping file."
-        else
-        
-                echo $fq1
-                echo $fq2
-                echo $base
-                ## Run trimmomatic
-                java -jar "$TRIMMOMATIC" PE $raw/$fq1 $raw/$fq2 $trim/${base}_R1p.trimmed.fastq.gz $trim/${base}_R1u.trimmed.fastq.gz $trim/${base}_R2p.trimmed.fastq.gz $trim/${base}_R2u.trimmed.fastq.gz ILLUMINACLIP:"$adapters":2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-        fi 
-done
+CHECKFILE="${base}_R1u.trimmed.fastq.gz"
+
+if [ -f $trim/$CHECKFILE ]; then
+        echo "$CHECKFILE exists. Skipping file."
+else
+        echo $fq1
+        echo $fq2
+        echo $base
+
+        ## Run trimmomatic
+        java -jar "$TRIMMOMATIC" PE -threads 40 $raw/$fq1 $raw/$fq2 $trim/${base}_R1p.trimmed.fastq.gz $trim/${base}_R1u.trimmed.fastq.gz $trim/${base}_R2p.trimmed.fastq.gz $trim/${base}_R2u.trimmed.fastq.gz ILLUMINACLIP:"$adapters":2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
 wait
-
-## FIN
