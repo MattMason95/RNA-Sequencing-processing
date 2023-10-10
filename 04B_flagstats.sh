ab3a06ddd934b0198cc63b5e1ce25afd97d8d713
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-10-10 15:42:49 +0100

    Create 04B_flagstats.sh

---
+++ 04B_flagstats.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# AUTHOR: Matthew Mason
+# EDITED: 10.2023
+# DESC: Full assembly pipeline of BASH scripts for RNA-seq read alignment and quantification
+
+echo "Generate Flag Stats of BAM files {04B_flagstats.sh}" # Declare start of trimming process
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+# <> BUILD ENVIRONMENT                          <>
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+
+# Always add this command to your scripts
+source $(conda info --base)/etc/profile.d/conda.sh
+
+conda activate bioinformatics
+
+echo $CONDA_DEFAULT_ENV
+
+## PASSED VARIABLES 
+sample="$1"
+
+## STATIC VARIABLES 
+ALIGNED="$HOME/rds/hpc-work/Data/Aligned_reads"    #Location to read trimmed reads
+BAI="$HOME/rds/hpc-work/Data/BAI"     #Location to write BAM files
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+# <> READ INDEXING                              <>
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+
+echo sample: "$sample"
+
+## FIND BAM FILE 
+for bam in $ALIGNED/$sample/*.out.bam;
+do
+
+base=$(basename "$bam")
+
+echo $bam
+echo $base
+
+cd $ALIGNED/$sample
+
+echo "$PWD"
+
+## EXECUTE
+cmd1="samtools flagstat $base"
+
+echo $cmd1
+eval $cmd1
+done
