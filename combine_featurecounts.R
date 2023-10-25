9170800d908b3c7e2a62a84ae8d7e112c83cec84
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-10-25 15:38:16 +0100

    Create combine_featurecounts.R

---
+++ combine_featurecounts.R
@@ -0,0 +1,30 @@
+# AUTHOR: Matthew Mason
+# EDITED: 10.2023
+# DESC: R script for generating a single FeatureCounts matrix for submission to DeSeq2 
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+# <> LIBRARY INSTANTIATION                      <>
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+
+library(magrittr)
+library(dplyr)
+library(purrr)
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+# <> SCRIPT                                     <>
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+
+## Fetch files matching pattern
+FILES<- list.files(pattern="*feature_counts.txt$", recursive=True, full.names=True)
+
+## Function for accessing the relevant columns of the individual files
+countMerger<- function(f){
+  counts<- read_tsv(f,col_names=True,comment=#)
+  counts<- counts %>% dplyr::select(-c(Chr,Start,End,Strand,Length))
+  return(counts)
+}
+
+rawCounts<- map(FILES, countMerger)
+countMatrix<- purrr::reduce(rawCounts,full_join)
+
+## FIN                  
