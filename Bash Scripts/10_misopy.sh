2084e065e4876b2e3f8c71ff6cf671a381a93161
MattMason95 <89090529+MattMason95@users.noreply.github.com>
2023-11-03 12:09:35 +0000

    Update 10_misopy.sh

--- Bash Scripts/10_misopy.sh
+++ Bash Scripts/10_misopy.sh
@@ -34,7 +34,7 @@ GTF="$HOME/rds/hpc-work/Data/ENSEMBL/"
 GFF3="Mus_musculus.GRCm39.110.gff3"
 
 if [ -f $GTF/$GFF3 ]; then
-   echo "GFF3 file - $FILE - already exists. Proceeding with MISO"
+   echo "GFF3 file - $GFF3 - already exists. Proceeding with MISO"
 else
    echo "GFF3 file does not exist. Converting GTF to GFF3"
    
@@ -43,37 +43,37 @@ else
    cd $GTF
    cmd1="agat_convert_sp_gxf2gxf.pl -g Mus_musculus.GRCm39.110.gtf -o $GFF3"
    echo $cmd1
-   eval $cmd1
+   #eval $cmd1
    wait
    [ -f $GFF3 ] && echo "GFF3 file created!" 
 fi
 
 ## GFF INDEXING
 mkdir -p indexed_gff
+
 cmd2="miso index_gff --index $GFF3 indexed_gff"
 echo $cmd2 
-eval $cmd2
+#eval $cmd2
 wait
 
 cd ../..
 echo "$PWD"
 
 # <><><><><><><><><><><><><><><><><><><><><><><><>
-# <> GFF INDEXING                              <>
+# <> GFF INDEXING                               <>
 # <><><><><><><><><><><><><><><><><><><><><><><><>
 
 cd Data
 mkdir -p miso_output
 cd miso_output
 
-BAM="$HOME/rds/hpc-work/Data/Aligned_reads/$sample/*.out.rd.bam"
+BAM="$HOME/rds/hpc-work/Data/Aligned_reads/$sample/*.out.rd.bam"i
 IndexDIR="$HOME/rds/hpc-work/Data/ENSEMBL/indexed_gff"
 
 cmd3="miso --run $IndexDIR $BAM --output-dir ./miso_output --read-len 150 --paired-end 250 15 --use-cluster"
 
 echo $cmd3 
-eval $cmd3
+#eval $cmd3
 done
 
 ## FIN 
-
